2b5854cc8ea57df1a91b01b62944775f57ae5d8b
Akashchauhan001 <akashchauhan191205@gmail.com>
2026-04-03 10:20:53 +0530

    [Akash] Stack Solutions added

--- Stack_Solutions/TwoStacksOneArray.java
+++ Stack_Solutions/TwoStacksOneArray.java
@@ -1,6 +1,4 @@
-package Stack;
-
-public class TwoStacksOneArray {
+public class TwoStacksOneArray{
 
     int size = 10;
     int arr[] = new int[size];
@@ -8,48 +6,41 @@ public class TwoStacksOneArray {
     int i = -1;
     int j = size;
 
-    void push1(int x)
-    {
-        if(i < j - 1)
-        {
+    void push1(int x) {
+        if (i < j - 1) {
             i++;
             arr[i] = x;
-        }
-        else
+        } else {
             System.out.println("Overflow");
+        }
     }
 
-    void push2(int x)
-    {
-        if(i < j - 1)
-        {
+    void push2(int x) {
+        if (i < j - 1) {
             j--;
             arr[j] = x;
-        }
-        else
+        } else {
             System.out.println("Overflow");
+        }
     }
 
-    int pop1()
-    {
-        if(i >= 0)
+    int pop1() {
+        if (i >= 0)
             return arr[i--];
 
         System.out.println("Underflow");
         return -1;
     }
 
-    int pop2()
-    {
-        if(j < size)
+    int pop2() {
+        if (j < size)
             return arr[j++];
 
         System.out.println("Underflow");
         return -1;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         TwoStacksOneArray s = new TwoStacksOneArray();
 
         s.push1(5);
